ee6d47cd73a7c54883a6e3b9b2997c9035b2af89
Amal Prakash <amalkj20000@gmail.com>
2023-08-12 19:35:52 +0530

    stage 4 checkpoint

--- mynitcbase/stage_3/Buffer/BlockBuffer.cpp
+++ mynitcbase/stage_3/Buffer/BlockBuffer.cpp
@@ -14,30 +14,40 @@ BlockBuffer::BlockBuffer(int blockNum)
 
 // calls the parent class constructor
 RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
+// the declarations for these functions can be found in "BlockBuffer.h"
 
-// load the block header into the argument pointer
+/*
+Used to get the header of the block into the location pointed to by `head`
+NOTE: this function expects the caller to allocate memory for `head`
+*/
 int BlockBuffer::getHeader(struct HeadInfo *head)
 {
-    unsigned char buffer[BLOCK_SIZE];
 
-    // read the block at this.blockNum into the buffer
-    Disk::readBlock(buffer, this->blockNum);
-
+    unsigned char *bufferPtr;
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+    if (ret != SUCCESS)
+    {
+        return ret; // return any errors that might have occured in the process
+    }
+    // rest of the logic is as in stage 2
     memset(head, 0, sizeof(struct HeadInfo));
     // populate the numEntries, numAttrs and numSlots fields in *head
-    memcpy(&head->reserved, buffer + 28, 4);
-    memcpy(&head->numSlots, buffer + 24, 4);
-    memcpy(&head->numAttrs, buffer + 20, 4);
-    memcpy(&head->numEntries, buffer + 16, 4);
-    memcpy(&head->rblock, buffer + 12, 4);
-    memcpy(&head->lblock, buffer + 8, 4);
-    memcpy(&head->pblock, buffer + 4, 4);
-    memcpy(&head->blockType, buffer, 4);
+    memcpy(&head->reserved, bufferPtr + 28, 4);
+    memcpy(&head->numSlots, bufferPtr + 24, 4);
+    memcpy(&head->numAttrs, bufferPtr + 20, 4);
+    memcpy(&head->numEntries, bufferPtr + 16, 4);
+    memcpy(&head->rblock, bufferPtr + 12, 4);
+    memcpy(&head->lblock, bufferPtr + 8, 4);
+    memcpy(&head->pblock, bufferPtr + 4, 4);
+    memcpy(&head->blockType, bufferPtr, 4);
 
     return SUCCESS;
 }
 
-// load the record at slotNum into the argument pointer
+/*
+Used to get the record at slot `slotNum` into the array `rec`
+NOTE: this function expects the caller to allocate memory for `rec`
+*/
 int RecBuffer::getRecord(union Attribute *rec, int slotNum)
 {
     struct HeadInfo head;
@@ -48,22 +58,52 @@ int RecBuffer::getRecord(union Attribute *rec, int slotNum)
     int attrCount = head.numAttrs;
     int slotCount = head.numSlots;
 
-    // read the block at this.blockNum into a buffer
-    unsigned char buffer[BLOCK_SIZE];
-    Disk::readBlock(buffer, this->blockNum);
-
+    unsigned char *bufferPtr;
+    int ret = loadBlockAndGetBufferPtr(&bufferPtr);
+    if (ret != SUCCESS)
+    {
+        return ret;
+    }
+    // ... (the rest of the logic is as in stage 2
     /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
        - each record will have size attrCount * ATTR_SIZE
        - slotMap will be of size slotCount
     */
+
     int recordSize = attrCount * ATTR_SIZE;
     int slotNumRecordOffset = (HEADER_SIZE + slotCount) + (recordSize* slotNum);
     /* calculate buffer + offset */;
-    unsigned char *slotPointer = buffer + slotNumRecordOffset;
+    unsigned char *slotPointer = bufferPtr + slotNumRecordOffset;
 
     // load the record into the rec data structure
-    memset(rec, 0, recordSize);
     memcpy(rec, slotPointer, recordSize);
+
+    return SUCCESS;
+}
+
+/*
+Used to load a block to the buffer and get a pointer to it.
+NOTE: this function expects the caller to allocate memory for the argument
+*/
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr)
+{
+    // check whether the block is already present in the buffer using StaticBuffer.getBufferNum()
+    int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
+
+    if (bufferNum == E_BLOCKNOTINBUFFER)
+    {
+        bufferNum = StaticBuffer::getFreeBuffer(this->blockNum);
+
+        if (bufferNum == E_OUTOFBOUND)
+        {
+            return E_OUTOFBOUND;
+        }
+
+        Disk::readBlock(StaticBuffer::blocks[bufferNum], this->blockNum);
+    }
+
+    // store the pointer to this buffer (blocks[bufferNum]) in *buffPtr
+    (*buffPtr) = StaticBuffer::blocks[bufferNum];
 
     return SUCCESS;
 }
